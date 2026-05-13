5da9bfea2b76b4fdb491f2bd72949ef057f8bec0
asberyl <asberylsheraphin@gmail.com>
2026-05-13 11:48:36 +0530

    Changes made in some code

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from enum import Enum
+
+class itemid(str, Enum):
+    cabbagekootu = "cabbagekootu"
+    greenchilli = "greenchilli"
+    curryleaves = "curryleaves"
+    onion = "onion"
+
+
 app = FastAPI()
-
 @app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/indian_kootu_receipe/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+
+    if q == "yes":
+        return {"message": f"{item_id} is Cleaned"}
+
+    return {"message": f"{item_id} is not cleaned"}
